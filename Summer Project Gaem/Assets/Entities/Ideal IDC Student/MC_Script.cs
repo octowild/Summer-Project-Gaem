ba45da7656872672e82e86fb948f053502ba6b79
octowild <139123949+octowild@users.noreply.github.com>
2023-07-11 21:57:45 +0530

    eeeeee

---
+++ Summer Project Gaem/Assets/Entities/Ideal IDC Student/MC_Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC_Script : MonoBehaviour
+{
+    public RigidBody rigidbody;
+    public float Gravity;
+
+    public Transform Transform { get => transform; set => transform = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
