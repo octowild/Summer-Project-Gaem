c99e7430bb31cef7bee21a354bccb16363f877ad
octowild <139123949+octowild@users.noreply.github.com>
2023-07-12 21:48:30 +0530

    bf

--- Summer Project Gaem/Assets/camfollowmc.cs
+++ Summer Project Gaem/Assets/camfollowmc.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class camfollowmc : MonoBehaviour
 {
-    public Transform player;
+    public RigidBody2D player;
     public Vector3 offset=new Vector3(0,0,-10);
     public bool istrig = false;
  
@@ -17,7 +17,8 @@ public class camfollowmc : MonoBehaviour
     {
         if (istrig)
         {
-            gameObject.transform.position = player.position + offset;
+
+            //gameObject.transform.position = player.position + offset;
         }
 
     }
@@ -25,8 +26,8 @@ public class camfollowmc : MonoBehaviour
     private void OnTriggerStay2D(Collider2D collision)
     {
         istrig = true;
-        offset = gameObject.transform.position - player.position;
-        offset = new Vector3(offset.x, 0, -10);
+       // offset = gameObject.transform.position - player.position;
+       // offset = new Vector3(offset.x, 0, -10);
         
     }
     private void OnTriggerExit2D(Collider2D Collision)
