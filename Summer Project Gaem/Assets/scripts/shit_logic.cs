ea15b8229f0bbcb848cd3bf721e0e5ce5fbfee62
octowild <139123949+octowild@users.noreply.github.com>
2023-07-16 18:53:26 +0530

    nsd

--- Summer Project Gaem/Assets/scripts/shit_logic.cs
+++ Summer Project Gaem/Assets/scripts/shit_logic.cs
@@ -8,27 +8,29 @@ public class shitlogic : MonoBehaviour
     public Animator anim;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    private float speed;
 
 
     void Start()
     {
-        
+        speed = fallspeed;
     }
 
 
     void Update()
     {
-        float speed = fallspeed;
+        
         if (IsGrounded())
         {
             speed = 0;
             anim.SetBool("splash",true);
+            
 
         }
         transform.position += Vector3.down * speed * Time.deltaTime;
     }
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
     }
 }
