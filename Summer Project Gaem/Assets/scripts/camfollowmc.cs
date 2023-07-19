d13063fa05aa820b9c4e0a75fede6d5a51ffb9dc
octowild <139123949+octowild@users.noreply.github.com>
2023-07-19 16:42:51 +0530

    fwwqf

--- Summer Project Gaem/Assets/scripts/camfollowmc.cs
+++ Summer Project Gaem/Assets/scripts/camfollowmc.cs
@@ -5,53 +5,53 @@ using UnityEngine;
 public class camfollowmc : MonoBehaviour
 {
     public GameObject player;
-    public MC_Script mcs;
-    public Rigidbody2D mcrb;
+ //   public MC_Script mcs;
+ //   public Rigidbody2D mcrb;
     public float speedfactor;
-    public Vector2 offset;
-    public float camspeed;
-    private Vector2 threshold;
+ //   public Vector2 offset;
+  //  public float camspeed;
+ //   private Vector2 threshold;
 
     void Start()
     {
-        threshold = calthreshold();
+ //       threshold = calthreshold();
     }
-    private void FixedUpdate()
+  
+    void FixedUpdate()
     {
+     //   camspeed = mcs.movespeed > speedfactor ? mcs.movespeed : speedfactor;
+     //   if (Mathf.Abs(player.transform.position.x - transform.position.x) >= offset.x || Mathf.Abs(player.transform.position.y - transform.position.y) >= offset.y)
+     //   {
+     //       transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z), camspeed * Time.deltaTime);
 
-        camspeed = mcs.movespeed > speedfactor ? mcs.movespeed : speedfactor;
-        if (Mathf.Abs(player.transform.position.x - transform.position.x) >= offset.x || Mathf.Abs(player.transform.position.y - transform.position.y) >= offset.y)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z), camspeed * Time.deltaTime);
+     //   }
 
-
-
-        }
-    }
-    void Update()
-    {
-       // Vector2 follow = player.transform.position;
-      //  float xdiff = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
-     //   float ydiff = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
-     //   Vector3 newpos = transform.position;
-     //   if (Mathf.Abs(xdiff) >= threshold.x)
-    //    {
-    //        newpos.x = follow.x;
-    //    }
-    //    if (Mathf.Abs(ydiff) >= threshold.y)
-    //    {
-     //       newpos.y = follow.y;
+     //   Vector2 follow = player.transform.position;
+  //      float xdiff = Vector2.Distance(Vector2.right * transform.position.x, Vector2.right * follow.x);
+   //     float ydiff = Vector2.Distance(Vector2.up * transform.position.y, Vector2.up * follow.y);
+  //      Vector3 newpos = transform.position;
+  //      if (Mathf.Abs(xdiff) >= threshold.x)
+  //      {
+  //          newpos.x = follow.x;
+  //      }
+   //     if (Mathf.Abs(ydiff) >= threshold.y)
+   //     {
+    //        newpos.y = follow.y;
     //    }
     //    float movespeed = mcrb.velocity.magnitude > camspeed ? mcrb.velocity.magnitude : camspeed;
-    //    transform.position = Vector3.MoveTowards(transform.position,newpos,movespeed*Time.deltaTime);
+   //     transform.position = Vector3.MoveTowards(transform.position,newpos,movespeed*Time.deltaTime);
+
+        Vector3 newpos=player.transform.position+new Vector3(0,0,transform.position.z);
+        Vector3 smoothpos = Vector3.Lerp(transform.position,newpos,speedfactor*Time.deltaTime);
+        transform.position = smoothpos;
     }
-    private Vector3 calthreshold()
-    {
-        Rect aspect = Camera.main.pixelRect;
-        Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
-        t.x -= offset.x;
-        t.y -= offset.y;
-        return t;
+ //   private Vector3 calthreshold()
+  //  {
+ //       Rect aspect = Camera.main.pixelRect;
+ //       Vector2 t = new Vector2(Camera.main.orthographicSize * aspect.width / aspect.height, Camera.main.orthographicSize);
+ //       t.x -= offset.x;
+ //       t.y -= offset.y;
+ //       return t;
     }
         
 
