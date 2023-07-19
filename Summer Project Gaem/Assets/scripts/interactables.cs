0f1ce996972f2388e3c7e330962bf8a08bb7b1e2
octowild <139123949+octowild@users.noreply.github.com>
2023-07-19 16:57:41 +0530

    wefef

--- Summer Project Gaem/Assets/scripts/interactables.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface interactables
-{
-    public string prompt { get; }
-    public bool interact(Logicmain interactor);
-}
---
+++ Summer Project Gaem/Assets/scripts/mcspawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mcspawn : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+    }
+}
