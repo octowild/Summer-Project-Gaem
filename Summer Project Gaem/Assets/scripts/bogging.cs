8dcd0eef8363375469838c0ee74023ce4177f7d7
octowild <139123949+octowild@users.noreply.github.com>
2023-07-25 20:13:32 +0530

    eeeeeeeeeee

--- Summer Project Gaem/Assets/scripts/bogging.cs
+++ Summer Project Gaem/Assets/scripts/bogging.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class bogging : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool boged;
+    public float speeddec;
+    public float jumpdec;
+    public MC_Script mc;
+
+    
     void Start()
     {
-        
+        mc = GameObject.FindGameObjectWithTag("Player").GetComponent<MC_Script>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        boged = true;
+        mc.movespeed /= speeddec;
+        mc.jumpstr /= jumpdec;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        boged = false;
+        mc.movespeed *= speeddec;
+        mc.jumpstr *= jumpdec;
     }
 }
