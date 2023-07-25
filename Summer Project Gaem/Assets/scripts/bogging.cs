2d90bf2695a98a1a5b284f9ac390958b7306ec49
octowild <139123949+octowild@users.noreply.github.com>
2023-07-25 20:27:54 +0530

    dsadsa

--- Summer Project Gaem/Assets/scripts/bogging.cs
+++ Summer Project Gaem/Assets/scripts/bogging.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class bogging : MonoBehaviour
 {
-    public bool boged;
+
     public float speeddec;
     public float jumpdec;
+    public int dmg;
+    public float inttimer;
+    public float ticktimer;
     public MC_Script mc;
+    public bool boged;
+    private float _tint;
+    private float _ttick;
 
-    
+
     void Start()
     {
         mc = GameObject.FindGameObjectWithTag("Player").GetComponent<MC_Script>();
@@ -18,7 +24,16 @@ public class bogging : MonoBehaviour
 
     void Update()
     {
-        
+        if (boged)
+        {
+            _tint += Time.deltaTime;
+            _ttick += Time.deltaTime;
+        }
+        if (_tint >= inttimer&&_ttick>=ticktimer)
+        {
+            mc.dmgtaken += dmg;
+            _ttick = 0;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -31,5 +46,6 @@ public class bogging : MonoBehaviour
         boged = false;
         mc.movespeed *= speeddec;
         mc.jumpstr *= jumpdec;
+        _tint = 0;
     }
 }
