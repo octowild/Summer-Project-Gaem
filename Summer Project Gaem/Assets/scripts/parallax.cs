96659e2583a4c2a4694d75340a10a8644c96e988
octowild <139123949+octowild@users.noreply.github.com>
2023-08-02 14:07:19 +0530

    hjvb

--- Summer Project Gaem/Assets/scripts/parallax.cs
+++ Summer Project Gaem/Assets/scripts/parallax.cs
@@ -7,16 +7,21 @@ public class parallax : MonoBehaviour
     private Vector2 length, startpos;
     public GameObject cam;
     public float paraeff;
+    public float paraeffy;
     void Start()
     {
-        startpos.x = transform.position.x;
+        startpos = transform.position;
         length.x = GetComponent<SpriteRenderer>().bounds.size.x;
+        length.y = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
 
     void Update()
     {
+        float temp = (cam.transform.position.x * (1 - paraeff));
         float dist = (cam.transform.position.x * paraeff);
         transform.position = new Vector3(startpos.x + dist, transform.position.y, transform.position.z);
+        if (temp > startpos.x + length.x) startpos.x += length.x;
+        else if (temp < startpos.x - length.x) startpos.x -= length.x;
     }
 }
